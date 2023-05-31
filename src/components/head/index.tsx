c4369a9901e351ad74eda8415cfea4bd08a2a6e1
ccedit <ccedit@126.com>
2023-05-31 20:26:21 +0800

    使用next重新创建

--- src/components/head/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Head() {
-  return <div className="Head">head</div>;
-}
-
-export default Head;
--- src/pages/login/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-function Login() {
-  return <div className="Login">login</div>;
-}
-
-export default Login;
