911cb75778a8259085724026edf2c216f08c33b4
ccedit <ccedit@126.com>
2023-06-01 11:54:00 +0800

    api

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -4,7 +4,7 @@ const buildEslintCommand = (filenames) =>
     `next lint --fix --file ${filenames
         .map((f) => path.relative(process.cwd(), f))
         .join(' --file ')}`;
-
+console.log('!!!!!!!!!!!!!!!!')
 module.exports = {
     'src/*.{js,jsx,ts,tsx}': [buildEslintCommand],
 };
