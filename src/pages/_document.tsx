10804276acf392f51e3f75f91ea4cd322cdbc0e0
ccedit <ccedit@126.com>
2023-06-03 14:05:53 +0800

    增加document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function _document() {
+  return (
+    <Html>
+      <Head>
+      </Head>
+      <body>
+      <Main />
+      <NextScript />
+      </body>
+    </Html>
+  );
+}
